ccfebc5ae7e062c265deb54c2f35cf25417309a2
Ani <ani@ani.(none)>
2014-05-11 14:37:54 +0400

    Author : Ani
    Added Transport file

---
+++ Transport/Ertuxayin.java
@@ -0,0 +1,13 @@
+
+// Author: Ani G.
+//Ays file-um bervac e Ertuxayin class-i nkaragrutyuny vory jarangum e Transport class-ic 
+
+public class Ertuxayin extends Transport{
+    public void tr() {
+        set_zbaxvac_texeri_qanak(9); 
+        set_tomsi_gin(150);
+        set_nstatexeri_qanak(15);
+    }
+
+
+}

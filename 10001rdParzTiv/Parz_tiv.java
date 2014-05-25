3821658895c0c90d211306ba567bf1f63b2a8948
Ani <ani@ani.(none)>
2014-05-26 00:42:19 +0400

    Author: Ani -ubdate TicTacToe/js/tictactoe.js

---
+++ 10001rdParzTiv/Parz_tiv.java
@@ -0,0 +1,16 @@
+// Author: Ani G.
+// Ays file-um nkaragrvac funkcian veradardznum e 0 arjeq, ete tivy parz e, 1 hakarak depqum
+
+public class Parz_tiv {
+    public int parz(int tiv){
+	int parza=1;
+       for (int i=2;  i <= tiv / 2; i++){
+
+            if(tiv % i == 0){
+                parza=0;
+                break;
+            }
+        }
+        return parza;
+    }
+}
